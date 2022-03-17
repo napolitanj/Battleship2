29256b4deb171deac4b9aed81cde974ba1a61c73
Joseph <napolitanjoe@gmail.com>
2022-03-17 17:41:03 -0400

    add players

---
+++ src/components/game.js
@@ -0,0 +1,12 @@
+import Gameboard from "../components/gameboard.js"
+import Player from "../components/player.js"
+
+
+//Initialize Game
+const Game = () => {
+    
+    //Create Human and Computer player
+    const player1 = Player("Player 1")
+    const computer = Player("Computer")
+
+}
