fa56cfdd2c68af5e6c03457b8c742ca29dc0377c
Joseph <napolitanjoe@gmail.com>
2022-03-17 11:58:20 -0400

    add ship and testing

--- src/components/ship.js
+++ src/components/ship.js
@@ -1,5 +1,39 @@
-function sum(a,b) {
-    return a+b
+const Ship = (length, name) => {
+    return {
+        length,
+        name,
+        hull: buildHull(length,),
+        heading:'horizontal',
+        getHeading() {
+            this.heading
+        },
+        changeHeading() {
+            if (this.heading === 'horizontal') {
+                this.heading = 'vertical'
+            } else {
+                this.heading = 'horizontal'
+            }
+        },
+        hit(position) {
+            this.hull[position] = 0;
+            return this.hull;
+        },
+        isSunk() {
+            if (this.hull.reduce((total, currentPositionValue) => total + currentPositionValue) === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
 
-export default sum;
+function buildHull(length) {
+    let hull = []
+    for (let i = 0; i < length; i++) {
+        hull.push(1);
+    }
+    return hull;
+}
+
+export default Ship;
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,5 +1,23 @@
-import sum from "../components/ship.js"
+import Ship from "../components/ship.js"
 
-test('adds 1 +2 to equal 3', () => {
-    expect(sum(1,2)).toBe(3);
+const cargo = Ship(2,"Cargo Ship")
+
+test('Ship has a name and a length', () => {
+    expect(cargo).toMatchObject({length:2,name:"Cargo Ship"});
 });
+
+test('Ship takes a hit', () => {
+    cargo.hit(1)
+    expect(cargo.hull).toEqual([1,0])
+});
+
+test('Ship does not sink if health is not 0', () => {
+    cargo.hit(1)
+    expect(cargo.isSunk()).toBe(false)
+})
+
+test('Ship sinks', () => {
+    cargo.hit(0);
+    cargo.hit(1);
+    expect(cargo.isSunk()).toBe(true)
+})
